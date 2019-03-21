880bf5f6bc0a852d17fd4d890122c6d0819c4687
wangze <wangze@huishoubao.com.cn>
2019-03-21 17:01:19 +0800

    add algorithm3

--- app/src/main/java/com/example/administrator/algorithmproject/Algorithm3.java
+++ app/src/main/java/com/example/administrator/algorithmproject/Algorithm3.java
@@ -1,7 +1,40 @@
 package com.example.administrator.algorithmproject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * 给定一个递增的数组，求和为s的数字有多少对
+ * 给定一个递增的数组，求和为s的两个数有多少对
  */
 public class Algorithm3 {
+
+    public static List<String> findResult(List<Integer> list, int sum){
+
+        List<String> resultList = new ArrayList<>();
+
+        if (list == null || list.size() < 2){
+            return null;
+        }
+
+        int startCount = 0;
+
+        while (startCount < list.size()){
+            int anArray = list.get(startCount);
+            for (int j = list.size() - 1; j >= startCount ; j--) {
+                if (anArray + list.get(j) < sum) {//一个数加上数组中最大的数仍然小于所求，则所有都不满足
+                    break;
+                } else {
+                    if (anArray + list.get(j) == sum) {
+                        resultList.add(anArray+"---"+list.get(j));
+                    }
+                }
+            }
+            startCount++;
+        }
+
+        return resultList;
+    }
+
 }
--- app/src/main/java/com/example/administrator/algorithmproject/MainActivity.java
+++ app/src/main/java/com/example/administrator/algorithmproject/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String str = "We are happy";
         Log.e("what", "onCreate: "+Algorithm2.replaceInvildeStr(str.toCharArray() , str.length()));
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0 ; i < 100 ; i ++){
+            list.add(i);
+        }
+        Algorithm3.findResult(list , 60);
     }
 }
